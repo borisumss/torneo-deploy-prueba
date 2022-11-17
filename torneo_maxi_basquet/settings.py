053a025c005b1ce1106b92ff3f0ff8f3d14e4f6d
borisumss <93559073+borisumss@users.noreply.github.com>
2022-11-17 11:21:19 -0400

    Update settings.py

--- torneo_maxi_basquet/settings.py
+++ torneo_maxi_basquet/settings.py
@@ -147,7 +147,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'dimelsa.soft@gmail.com'
-EMAIL_HOST_PASSWORD = 'iumdadaygfqwjibi'
+EMAIL_HOST_PASSWORD = 'ydAA!K42%8A5'
 EMAIL_USE_TLS = True
 
 CSRF_TRUSTED_ORIGINS = ['https://web-production-0910.up.railway.app']
