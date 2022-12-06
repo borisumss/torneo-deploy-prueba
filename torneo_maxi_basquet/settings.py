634b63e2acfd22482076b8ced671a8772a287e0d
borisumss <93559073+borisumss@users.noreply.github.com>
2022-12-06 17:44:50 -0400

    email fake

--- torneo_maxi_basquet/settings.py
+++ torneo_maxi_basquet/settings.py
@@ -148,10 +148,14 @@ EMAIL_USE_TLS = True
 #'''
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 #EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'dimelsa.softbolivia@gmail.com'
-EMAIL_HOST_PASSWORD = 'htkmjljbjifxwbei'
+EMAIL_HOST = 'smtp.mailtrap.io'
+EMAIL_HOST_USER = '1d93c2eceff900'
+EMAIL_HOST_PASSWORD = 'c0222687d20629'
+EMAIL_PORT = '2525'
+#EMAIL_HOST = 'smtp.gmail.com'
+#EMAIL_PORT = 587
+#EMAIL_HOST_USER = 'dimelsa.softbolivia@gmail.com'
+#EMAIL_HOST_PASSWORD = 'htkmjljbjifxwbei'
 EMAIL_USE_TLS = True
 #'''
 ######
