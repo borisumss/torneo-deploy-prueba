b465f8852201ab0915e59f04e2a548187acc0cd5
Boris Calli <201900823@est.umss.edu>
2022-12-07 21:47:40 -0400

    django mysql

--- torneo_maxi_basquet/settings.py
+++ torneo_maxi_basquet/settings.py
@@ -74,15 +74,40 @@ WSGI_APPLICATION = 'torneo_maxi_basquet.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
-
+'''
+'''
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'railway',
+        'USER': 'root',
+        'PASSWORD': 'hJ0jh3Vubqy9Qwr2PAfF',
+        'HOST': 'containers-us-west-157.railway.app',
+        'PORT': '6782',
+    }
+}
+'''
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    },
+    'new': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'railway',
+        'USER': 'root',
+        'PASSWORD': 'hJ0jh3Vubqy9Qwr2PAfF',
+        'HOST': 'containers-us-west-157.railway.app',
+        'PORT': '6782',
+    }
+}
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 
